113473cf952c9abe5130c415746689441e85b382
BarenAlen <foxfamer@gmail.com>
2015-06-17 01:21:56 +0300

    Final commit

---
+++ inc/header.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Fabriq</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+    <header class="header">
+      <div class="row">
+        <div class="col-md-3">
+          <h1>FABRIQUE<br/><span class="logo-text-header">FINE FUNISHINGS</span></h1>
+        </div>
+        <div class="col-md-5 col-md-offset-1">
+          <ul class="list-inline">
+            <li><a href="#">Log In</a></li>
+            <li><a href="#">Register</a></li>
+            <li><a href="#">Whishlist</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Currency</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <div class="mg-top-caret">
+            <img class="pull-left" src="img/bag black.png">
+            <div>
+              <span>WELCOME&nbsp;<a href="#">YOURNAME</a></span><br/>
+              <a href="#">8</a>&nbsp;items
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </header>
+      <nav class="navbar">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">HOME</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">NEW IN<span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+              <li><a href="#">SALE</a></li>
+              <li><a href="#">FLOORING</a></li>
+              <li><a href="#">CURTAINS</a></li>
+              <li><a href="#">BEDDING</a></li>
+              <li><a href="#">CONTACT US</a></li>
+            </ul>
+            <form class="navbar-form navbar-right">
+              <div class="input-group text">
+                <input type="text" class="form-control" placeholder="Search...">
+                <span class="input-group-btn">
+                    <button class="btn" type="button"><i class="fa fa-search"></i></button>
+                </span>
+              </div>
+          </form>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
---
+++ index.php
@@ -0,0 +1,130 @@
+
+<?php include('inc/header.php'); ?>
+ 
+<div class="wrapper">
+
+<section>
+	<div class="row">
+		<div class="col-md-9 padding-bottom box-showcase hidden-xs">
+			<img src="img/slider-img.jpg">
+			<div class="showcase-text">
+				<h3>CLAIRE&nbsp;AITCH&nbsp;LINEN</h3>
+				<p>Indulge yourself with the very best 100% cotton towels and linen by top UK designer<br/> Claire Aitch.<br/>Exclusive!</p>
+				<a href="product.php">Preview</a>
+			</div>
+		</div>
+		<div class="col-md-3 padding-top-bottom box-sider hidden-sm hidden-xs">
+			<img src="img/sidebar.jpg">
+			<div>
+				<img src="img/news.png" class="news-icon">
+				<p>Subscribe to out free monthly newsletter and be the first to hear about our offer.</p>	
+			</div>
+			<div class="input-group text">
+                <input type="text" class="form-control" placeholder="Email">
+                <span class="input-group-btn">
+                    <button class="btn" type="button"><i class="fa fa-angle-right"></i></button>
+                </span>
+             </div>
+		</div>
+	</div>
+</section>
+
+<section class="shelf border-top padding-bottom">
+	<h3>NEW PRODUCTS</h3>
+	<div class="row">
+		<div class="col-md-3 col-sm-6 pad">
+			<img src="img/new-product1.jpg">
+			<a href="#" class="link-md">Royale&nbsp;Curtains&nbsp;<span class="pull-right">$159.99</span></a>
+			<p>Sed rutrum ac lacus a malesuada. 
+				Duis at elit venenatis, lobortis metus in, 
+				semper felis. Maecenas consequat vulputate venenatis.
+			</p>
+			<a href="#" class="btn btn-default">View</a>
+			<a href="#" class="btn btn-default">Add To Basket</a>
+		</div>
+		<div class="col-md-3 col-sm-6 pad">
+			<img src="img/new-product2.jpg">
+			<a href="#" class="link-md">Claire&nbsp;Aitch&nbsp;Zebra&nbsp;Rug&nbsp;<span class="pull-right">$129.99</span></a>
+			<p>Sed rutrum ac lacus a malesuada. 
+				Duis at elit venenatis, lobortis metus in, 
+				semper felis. Maecenas consequat vulputate venenatis.
+			</p>
+			<a href="#" class="btn btn-default">View</a>
+			<a href="#" class="btn btn-default">Add To Basket</a>
+		</div>
+		<div class="col-md-3 col-sm-6 pad">
+			<img src="img/new-product3.jpg">
+			<a href="#" class="link-md">Claire&nbsp;Real&nbsp;Pine&nbsp;Wood&nbsp;Flooring&nbsp;<span class="pull-right">$12.99</span></a>
+			<p>Sed rutrum ac lacus a malesuada. 
+				Duis at elit venenatis, lobortis metus in, 
+				semper felis. Maecenas consequat vulputate venenatis.
+			</p>
+			<a href="#" class="btn btn-default">View</a>
+			<a href="#" class="btn btn-default">Add To Basket</a>
+		</div>
+		<div class="col-md-3 col-sm-6 pad">
+			<img src="img/new-product4.jpg">
+			<a href="#" class="link-md">Modern&nbsp;Flowers&nbsp;Bed&nbsp;Set&nbsp;<span class="pull-right">$87.99</span></a>
+			<p>Sed rutrum ac lacus a malesuada. 
+				Duis at elit venenatis, lobortis metus in, 
+				semper felis. Maecenas consequat vulputate venenatis.
+			</p>
+			<a href="#" class="btn btn-default">View</a>
+			<a href="#" class="btn btn-default">Add To Basket</a>
+		</div>
+	</div>
+
+</section>
+
+<section class="shelf border-top border-bottom padding-bottom ">
+	<h3>FEATURED&nbsp;PRODUCTS</h3>
+	<div class="row">
+		<div class="col-md-3 col-sm-6 pad">
+			<img src="img/featured-product1.jpg">
+			<a href="#" class="link-md">Royale&nbsp;Curtains&nbsp;<span class="pull-right">$59.99</span></a>
+			<p>Sed rutrum ac lacus a malesuada. 
+				Duis at elit venenatis, lobortis metus in, 
+				semper felis. Maecenas consequat vulputate venenatis.
+			</p>
+			<a href="#" class="btn btn-default">View</a>
+			<a href="#" class="btn btn-default">Add To Basket</a>
+		</div>
+		<div class="col-md-3 col-sm-6 pad">
+			<img src="img/featured-product2.jpg">
+			<a href="#" class="link-md">Claire&nbsp;Aitch&nbsp;Zebra&nbsp;Rug&nbsp;<span class="pull-right">$29.99</span></a>
+			<p>Sed rutrum ac lacus a malesuada. 
+				Duis at elit venenatis, lobortis metus in, 
+				semper felis. Maecenas consequat vulputate venenatis.
+			</p>
+			<a href="#" class="btn btn-default">View</a>
+			<a href="#" class="btn btn-default">Add To Basket</a>
+		</div>
+		<div class="col-md-3 col-sm-6 pad">
+			<img src="img/featured-product3.jpg">
+			<a href="#" class="link-md">Kay&nbsp;Lea&nbsp;Modern&nbsp;Bed&nbsp;Set&nbsp;<span class="pull-right">$59.99</span></a>
+			<p>Sed rutrum ac lacus a malesuada. 
+				Duis at elit venenatis, lobortis metus in, 
+				semper felis. Maecenas consequat vulputate venenatis.
+			</p>
+			<a href="#" class="btn btn-default">View</a>
+			<a href="#" class="btn btn-default">Add To Basket</a>
+		</div>
+		<div class="col-md-3 col-sm-6 pad">
+			<img src="img/featured-product4.jpg">
+			<a href="#" class="link-md">Ali&nbsp;Baba&nbsp;Rug&nbsp;&&nbsp;Custom&nbsp;<span class="pull-right">$229.99</span></a>
+			<p>Sed rutrum ac lacus a malesuada. 
+				Duis at elit venenatis, lobortis metus in, 
+				semper felis. Maecenas consequat vulputate venenatis.
+			</p>
+			<a href="#" class="btn btn-default">View</a>
+			<a href="#" class="btn btn-default">Add To Basket</a>
+		</div>
+	</div>
+
+</section>
+
+</div>
+
+<?php include('inc/footer.php'); ?>
+
+
